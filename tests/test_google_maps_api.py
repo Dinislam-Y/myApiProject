dc22b370c47dba246ae73efb7785d15fdd6e41ee
dinislam <977stalker47>
2023-05-11 02:41:50 +0300

    added put method

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -4,12 +4,17 @@ from utils.api import CoogleMapsApi
 
 
 class TestCreatePlace:
-    def test_create_new_place(self):
+    def test_api(self):
         print('\nМетод POST')
-        result_post: Response = CoogleMapsApi.create_nem_pleace()
+        result_post: Response = CoogleMapsApi.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
 
-        print('\nМетод GET')
-        result_get: Response = CoogleMapsApi.get_nem_pleace(place_id)
-        print(result_get)
+        print('\nМетод GET POST')
+        result_get: Response = CoogleMapsApi.get_new_place(place_id)
+
+        print('\nМетод PUT')
+        result_put: Response = CoogleMapsApi.put_new_place(place_id)
+
+        print('\nМетод GET PUT')
+        result_get: Response = CoogleMapsApi.get_new_place(place_id)
--- utils/api.py
+++ utils/api.py
@@ -1,41 +1,39 @@
 from utils.http_methods import HttpMethods
+from utils.data import TestDataJson
 
 base_url = "https://rahulshettyacademy.com"
 key = "?key=qaclick123"
-post_resource = "/maps/api/place/add/json"
-get_responce = "/maps/api/place/get/json"
+post_resource = "/maps/api/place/add/json"  # Ресурс метода Post
+get_resource = "/maps/api/place/get/json"  # Ресурс метода Get
+put_resource = "/maps/api/place/update/json"  # Ресурс метода Put
+testDataJson = TestDataJson.testDataJson
 
 
 class CoogleMapsApi:
 
+    # Метод для создания новой локации
     @staticmethod
-    def create_nem_pleace():
-        json_for_create_new_place = {
-            "location": {
-                "lat": -38.383494,
-                "Ing": 33.427362
-            }, "accuracy": 50,
-            "name": "Frontline house",
-            "phone number": "(+91) 983 893 3937",
-            "address": "29, sidelayout, cohen 09",
-            "types": [
-                "shoe park",
-                "shop"
-            ],
-            "website": "http://google.com",
-            "language": "French-IN"
-        }
-
+    def create_new_place():
         post_url = base_url + post_resource + key
-        result_post = HttpMethods.post(post_url, json_for_create_new_place)
-
+        print(post_url)
+        result_post = HttpMethods.post(post_url, testDataJson)
         print(result_post.text)
         return result_post
 
+    # Метод для проверки новой локации
     @staticmethod
-    def get_nem_pleace(place_id):
-        get_url = base_url + get_responce + key + "&place_id=" + place_id
+    def get_new_place(place_id):
+        get_url = base_url + get_resource + key + "&place_id=" + place_id
+        print(get_url)
         result_get = HttpMethods.get(get_url)
-
         print(result_get.text)
         return result_get
+
+    # Метод для проверки новой локации
+    @staticmethod
+    def put_new_place(place_id):
+        get_url = base_url + put_resource + key + "&place_id=" + place_id
+        print(get_url)
+        result_put = HttpMethods.put(get_url, place_id)
+        print(result_put.text)
+        return result_put
